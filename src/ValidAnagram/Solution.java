993e115c738b5ab94e48c8b20d53abc1bf35ac59
James Newman <n01490221@unf.edu>
2024-05-05 22:04:23 -0400

    Valid Anagram

---
+++ src/ValidAnagram/Solution.java
@@ -0,0 +1,29 @@
+package ValidAnagram;
+
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "aacc";
+        String t = "ccac";
+        System.out.println(isAnagram(s, t));
+    }
+    public static boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+
+        for (int num : map.values()) {
+            if (num != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
